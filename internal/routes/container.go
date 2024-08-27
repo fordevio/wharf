27512771c5ce3764c96bac5a5642cd3e0959ce32
YASH PATEL <yp969803@gmail.com>
2024-08-27 19:06:13 +0530

    rfac: container routes

--- internal/routes/container.go
+++ internal/routes/container.go
@@ -10,7 +10,7 @@ func ContainerRoutes(incommingRoutes *gin.RouterGroup) {
 	incommingRoutes.PUT("/container/stop/:id", controllers.StopContainer())
 	incommingRoutes.PUT("/container/unpause/:id", controllers.UnpauseContainer())
 	incommingRoutes.DELETE("/container/remove/:id", controllers.RemoveContainer())
-	incommingRoutes.DELETE("api/container/prune", controllers.PruneContainers())
+	incommingRoutes.DELETE("/container/prune", controllers.PruneContainers())
 	incommingRoutes.GET("/container/stats/:id", controllers.ContainerStats())
 	incommingRoutes.GET("/container/logs/:id", controllers.ContainerLogs())
 	incommingRoutes.GET("/container/rename/:id", controllers.ContainerRename())
