4124d01bf970e6f6b91f7c04f2b47d843e5496f8
YASH PATEL <yp969803@gmail.com>
2024-08-30 16:51:53 +0530

    rfac: network create route

--- internal/routes/network.go
+++ internal/routes/network.go
@@ -10,6 +10,6 @@ func NetworkRoutes(incommingRoutes *gin.RouterGroup) {
 	incommingRoutes.DELETE("/network/prune", controllers.PruneNetwork())
 	incommingRoutes.PUT("/network/disconnect/:id", controllers.DisconnectNetwork())
 	incommingRoutes.PUT("/network/connect/:id", controllers.ConnectNetwork())
-	incommingRoutes.POST("/network/create", controllers.ContainerCreate())
+	incommingRoutes.POST("/network/create", controllers.CreateNetwork())
 	incommingRoutes.DELETE("/network/remove/:id", controllers.RemoveNetwork())
 }
