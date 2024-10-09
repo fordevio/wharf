c43da99a8b0ecb9aae545e2f515cbff3ce7cdfd3
HarshikaAdarsh <Harshikaadarsh04@gmail.com>
2024-10-10 01:35:59 +0530

    fixed formatting issues in files

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import './index.css';
 import wharfLogo from '../../assets/wharf.png';
-import { useContext , useState } from 'react';
+import { useContext, useState } from 'react';
 import UserContext from '../../context/user/userContext';
 import { Link, useLocation } from 'react-router-dom';
 const Navbar = () => {
@@ -12,22 +12,22 @@ const Navbar = () => {
   if (userContext == null || userContext.curUser == null) {
     return <></>;
   }
-  const [menuOpen,  setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="nav">
       <img src={wharfLogo} className="logo" alt="wharf" />
-      
+
       <div
-  className={`menu ${menuOpen ? 'open' : ''}`}
-  onClick={() => {
-    setMenuOpen(!menuOpen);
-  }}
->
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-      <ul className={menuOpen ? "open" : ""}>
+        className={`menu ${menuOpen ? 'open' : ''}`}
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? 'open' : ''}>
         <li>
           <Link
             to="/"
@@ -36,7 +36,6 @@ const Navbar = () => {
           >
             Containers
           </Link>
-         
         </li>
         <li>
           <Link
