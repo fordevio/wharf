d65c9cd639671e1c783112dc30606a80217e1885
Yash Patel <yp969803@gmail.com>
2025-06-12 06:21:01 +0530

    feat: added containerNetworkCOnnect in frontend

--- client/src/pages/ContainerInside/Network/index.tsx
+++ client/src/pages/ContainerInside/Network/index.tsx
@@ -4,12 +4,17 @@ import { DockerContainer } from '../../../models/container';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getAllContainers } from '../../../api/container';
 import { useQuery } from 'react-query';
-import { disconnectContainerFromNetwork } from '../../../api/network';
+import {
+  connectContainerToNetwork,
+  disconnectContainerFromNetwork,
+} from '../../../api/network';
 import toast from 'react-hot-toast';
 
 const ContainerNetworks = () => {
   const [container, setContainer] = useState<DockerContainer | null>(null);
   const [openDl, setOpenDl] = useState(false);
+  const [openCn, setOpenCn] = useState(false);
+  const [conNetId, setConNetId] = useState('');
   const navigate = useNavigate();
   const [force, setForce] = useState<boolean>(false);
   const { id } = useParams();
@@ -68,6 +73,41 @@ const ContainerNetworks = () => {
     });
   };
 
+  const connectNet = async () => {
+    if (container === null || conNetId === '') {
+      return;
+    }
+    try {
+      const res = await connectContainerToNetwork(
+        localStorage.getItem('token') as string,
+        conNetId,
+        container?.Id
+      );
+      setOpenCn(false);
+      return res.data;
+    } catch (e: any) {
+      throw e.response ? e.response.data : { error: 'Request failed' };
+    }
+  };
+
+  const ConnectHandler = async () => {
+    if (conNetId === '') {
+      toast.error('Please fill all fields');
+      return;
+    }
+    toast.promise(connectNet(), {
+      loading: 'Connecting to network...',
+      success: data => {
+        fetchContainer();
+        return `Successfully connected to network `;
+      },
+      error: error => {
+        setOpenCn(false);
+        return `${error.error}`;
+      },
+    });
+  };
+
   useQuery('container' + id, fetchContainer, {
     retry: false,
   });
@@ -97,6 +137,11 @@ const ContainerNetworks = () => {
           <div className="title mode">
             Network Mode: {container?.HostConfig.NetworkMode}
           </div>
+          <div className=" connect-btn">
+            <button className="btn cn" onClick={() => setOpenCn(true)}>
+              Connect Network
+            </button>
+          </div>
           <div className="title">Network Settings</div>
           <div className="port-container">
             {Object.entries(container?.NetworkSettings.Networks || {}).map(
@@ -119,7 +164,7 @@ const ContainerNetworks = () => {
                   </div>
                   <div>
                     <button className="btn" onClick={() => setOpenDl(true)}>
-                      Disconnect{' '}
+                      Disconnect
                     </button>
                   </div>
                   <div
@@ -160,6 +205,34 @@ const ContainerNetworks = () => {
                 </div>
               )
             )}
+          </div>
+        </div>
+        <div
+          className="popup-overlay"
+          id="popupOverlay"
+          style={openCn ? { display: 'block' } : { display: 'none' }}
+        >
+          <div className="popup" id="popup">
+            <span
+              className="close"
+              id="closePopup"
+              onClick={() => setOpenCn(false)}
+            >
+              &times;
+            </span>
+
+            <div className="popup-content">
+              <input
+                type="text"
+                placeholder="Network ID"
+                value={conNetId}
+                onChange={e => setConNetId(e.target.value)}
+              />
+
+              <button className="submit" onClick={() => ConnectHandler()}>
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
