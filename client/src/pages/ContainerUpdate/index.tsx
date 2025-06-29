b165cf17ef3f45450595798255887e8e4e2daecc
Yash Patel <yp969803@gmail.com>
2025-06-29 18:12:46 +0530

    rfac: listCOntainers and stopContainer funnc

---
+++ client/src/pages/ContainerUpdate/index.tsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom';
+import './index.css'
+
+const ContainerUpdate = () => {
+    const {id} = useParams();
+    
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default ContainerUpdate
